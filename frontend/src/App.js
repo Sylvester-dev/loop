4c914b660c2ce4bd58bb87b647b6b38381311ad1
Hritwik Tripathi <tripathi.hritwik@gmail.com>
2021-11-22 00:25:19 +0530

    navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useMoralis } from "react-moralis";
+
+
 function App() {
+
+  const { authenticate, user, logout } = useMoralis();
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav" style={{}}>
+        {!user ? (
+          <>
+            <div className="bar">
+              <h3 className="inner" style={{ color: "#fed456" }}>
+                {!user ? "not logged in" : user.id}
+              </h3>
+              <button
+                onClick={() => {
+                  authenticate({
+                    onComplete: () => alert("You are now Logged in! 🎉"),
+                  });
+                }}
+                style={{
+                  height: "40px",
+                  marginTop: "10px",
+                  width: "20%",
+                  marginLeft: "25%",
+                  borderRadius: "50px",
+                }}
+                className="inner"
+              >
+                Login
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="bar">
+            <h2 className="inner" style={{color:"#ffffff"}}>loop</h2>
+              <h3
+                className="inner"
+                style={{ color: "#fed456" }}
+              >
+                {!user ? "not logged in" : user.id}
+              </h3>
+              <button
+                onClick={() => {
+                  logout({ onComplete: () => alert("Logged Out!") });
+                }}
+                style={{
+                  height: "40px",
+                  marginTop: "15px",
+                  width: "180px",
+                  
+                  borderRadius: "50px",
+                }}
+                className="inner"
+              >
+                Logout
+              </button>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
